16654b6148e08fd5c1f5b760890f0d9b7dfb8418
Malachi <76394091+malachis1447@users.noreply.github.com>
2023-02-22 06:08:44 -0600

    Update STIG-OL8.sh

--- OracleLinux/OL8/STIG-OL8.sh
+++ OracleLinux/OL8/STIG-OL8.sh
@@ -7,21 +7,23 @@ STIG_PROFILE="stigprofile-rhel8-ds.xml"
 if ! command -v oscap &> /dev/null
 then
     echo "Installing OpenSCAP tools..."
-    sudo yum install -y openscap-scanner
+    sudo dnf install -y openscap-scanner
 fi
 
 # Run the SCAP scan with the DISA STIG profile
 echo "Running SCAP scan..."
 oscap xccdf eval \    
 	--profile xccdf_org.ssgproject.content_profile_stig \
-	--results scan-results.xml \
-	--report scan-report.html \
+	--results ~/scan-results-ol8.xml \
+	--report ~/scan-report-ol8.html \
 	/usr/share/xml/scap/ssg/content/ssg-ol8-ds.xml
 
 # Check if any vulnerabilities were found
 if grep -q "fail" scan-results.xml
 then
     echo "Vulnerabilities found!"
+    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/ "scan-report-ol8.html"
 else
     echo "No vulnerabilities found."
+    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/ "scan-report-ol8.html"
 fi
