c2a0b3e14060601de226296763b90ed37839303d
Malachi <76394091+malachis1447@users.noreply.github.com>
2023-02-22 06:16:21 -0600

    Fix issue where script wouldn't complete
    Fix issue with indentation that would prevent the script from running

--- OracleLinux/OL8/STIG-OL8.sh
+++ OracleLinux/OL8/STIG-OL8.sh
@@ -12,18 +12,14 @@ fi
 
 # Run the SCAP scan with the DISA STIG profile
 echo "Running SCAP scan..."
-oscap xccdf eval \    
-	--profile xccdf_org.ssgproject.content_profile_stig \
-	--results ~/scan-results-ol8.xml \
-	--report ~/scan-report-ol8.html \
-	/usr/share/xml/scap/ssg/content/ssg-ol8-ds.xml
+oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig --results ~/scan-results-ol8.xml --report ~/scan-report-ol8.html /usr/share/xml/scap/ssg/content/ssg-ol8-ds.xml
 
 # Check if any vulnerabilities were found
 if grep -q "fail" scan-results.xml
 then
     echo "Vulnerabilities found!"
-    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/ "scan-report-ol8.html"
+    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/"scan-report-ol8.html"
 else
     echo "No vulnerabilities found."
-    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/ "scan-report-ol8.html"
+    echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/"scan-report-ol8.html"
 fi
