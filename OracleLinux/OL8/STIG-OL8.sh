54d409cd5933b574f16f45fe1e9292264412d3f9
Malachi <76394091+malachis1447@users.noreply.github.com>
2023-02-22 06:19:35 -0600

    Update STIG-OL8.sh

--- OracleLinux/OL8/STIG-OL8.sh
+++ OracleLinux/OL8/STIG-OL8.sh
@@ -12,7 +12,7 @@ echo "Running SCAP scan..."
 oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig --results ~/scan-results-ol8.xml --report ~/scan-report-ol8.html /usr/share/xml/scap/ssg/content/ssg-ol8-ds.xml
 
 # Check if any vulnerabilities were found
-if grep -q "fail" scan-results.xml
+if grep -q "fail" scan-results-ol8.xml
 then
     echo "Vulnerabilities found!"
     echo "The Results have been saved into an HTML report viewable in your web browser at " ~/stig-checks/"scan-report-ol8.html"
